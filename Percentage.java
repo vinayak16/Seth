f13b2d2f8ff28c2e19f0e4ca3b5ae7d784df5c2c
vinayak16 <rupeshseth16@gmail.com>
2019-04-10 11:50:55 +0530

    Percentage.java

---
+++ Percentage.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Percentage
+{
+	public static void main(String []arges)
+	{
+		int p;
+
+		Scanner scan =new Scanner(System.in);
+		System.out.println("enter the percentage : ");
+		p=scan.nextInt();
+ 		
+		if(p>=75)
+		System.out.println("first class with division");
+
+		else if(p>=60)
+		System.out.println("first class ");
+
+		else if(p>=50)
+		System.out.println("secound class");
+
+		else if(p<=40)
+		System.out.println("failed");
+
+	}
+}
